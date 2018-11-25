ef0d047b183fe1f3411f0c1f74440e146c094a64
Robine <gildas.robine@etu.emse.fr>
2018-11-25 19:39:26 +0100

    Ajout Google Maps
    Rajout de GMaps dans le frag 2 ( centré sur Aix pour le moment)

--- app/src/main/java/com/robine/gildas/wheretobeer/MapsFragment.java
+++ app/src/main/java/com/robine/gildas/wheretobeer/MapsFragment.java
@@ -6,14 +6,78 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 public class MapsFragment extends Fragment {
-    //Overriden method onCreateView
-    
+
+    MapView mMapView;
+    private GoogleMap googleMap;
+
+    //Override method onCreateView
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.mapsfragment_layout, container, false);
+
+        mMapView = (MapView) rootView.findViewById(R.id.mapView);
+        mMapView.onCreate(savedInstanceState);
+
+        mMapView.onResume();
+
+        try{
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        }catch( Exception e){
+            e.printStackTrace();
+        }
+
+        mMapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap mMap) {
+                googleMap = mMap;
+
+                // For dropping a marker at a point on the Map
+                LatLng aix = new LatLng(43.529742, 5.447427 );
+                googleMap.addMarker(new MarkerOptions().position(aix).title("Marker Title").snippet("Marker Description"));
+
+                // For zooming automatically to the location of the marker
+                CameraPosition cameraPosition = new CameraPosition.Builder().target(aix).zoom(12).build();
+                googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+            }
+        });
 
         //Returning the layout file after inflating
         //Change R.layout.tab1 in you classes
-        return inflater.inflate(R.layout.mapsfragment_layout, container, false);
+        return rootView;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
     }
 }
