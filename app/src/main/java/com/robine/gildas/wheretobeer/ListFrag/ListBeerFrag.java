6aa854d13e4ccb87c6ce8835d9c02f8803a851b3
Robine <gildas.robine@etu.emse.fr>
2018-11-25 17:30:23 +0100

    2 Onglets vides
    Custom view pour les onglets (icon+text)
    Pas de backend pour le moment

--- app/src/main/java/com/robine/gildas/wheretobeer/ListFrag/ListBeerFrag.java
+++ app/src/main/java/com/robine/gildas/wheretobeer/ListFrag/ListBeerFrag.java
@@ -4,23 +4,25 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.robine.gildas.wheretobeer.Beer;
 import com.robine.gildas.wheretobeer.R;
+
+import java.util.ArrayList;
 
 
 public class ListBeerFrag extends Fragment {
-    public static final String ARG_OBJECT = "object";
-
+    //Overriden method onCreateView
     @Override
-    public View onCreateView(LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View rootView = inflater.inflate(
-                R.layout.fragment_frag_obj1, container, false);
-        return rootView;
+        //Returning the layout file after inflating
+        //Change R.layout.tab1 in you classes
+        return inflater.inflate(R.layout.fragment_frag_obj1, container, false);
     }
 }
