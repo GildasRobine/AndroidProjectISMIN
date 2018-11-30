54ab6513ac79fa54f03a2c402fbac262d4f308d5
Robine <gildas.robine@etu.emse.fr>
2018-11-30 21:11:16 +0100

    Fin

--- app/src/main/java/com/robine/gildas/wheretobeer/InfoFrag.java
+++ app/src/main/java/com/robine/gildas/wheretobeer/InfoFrag.java
@@ -1,26 +1,14 @@
 package com.robine.gildas.wheretobeer;
 
-import android.content.Context;
-import android.net.Uri;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
-import com.google.firebase.database.DatabaseReference;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link InfoFrag.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link InfoFrag#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class InfoFrag extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
